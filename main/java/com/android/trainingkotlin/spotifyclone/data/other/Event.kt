283616269dd803dce3786e574a9c2b9d51803f49
Omar <42972288+OmarDroid@users.noreply.github.com>
2021-09-22 14:42:34 +0200

    Add files via upload

---
+++ main/java/com/android/trainingkotlin/spotifyclone/data/other/Event.kt
@@ -0,0 +1,18 @@
+package com.android.trainingkotlin.spotifyclone.data.other
+
+open class Event<out T>(private val data: T) {
+
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if(hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            data
+        }
+    }
+
+    fun peekContent() = data
+}
