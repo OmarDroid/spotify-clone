e532472628dcc26d3db9cd4b5fbcef00268e76ae
Omar <42972288+OmarDroid@users.noreply.github.com>
2021-10-10 16:37:58 +0200

    Delete main directory

--- main/java/com/android/trainingkotlin/spotifyclone/SpotifyApplication.kt
+++
@@ -1,7 +0,0 @@
-package com.android.trainingkotlin.spotifyclone
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SpotifyApplication : Application()
--- main/java/com/android/trainingkotlin/spotifyclone/data/entities/Song.kt
+++
@@ -1,9 +0,0 @@
-package com.android.trainingkotlin.spotifyclone.data.entities
-
-data class Song (
-    val mediaId: String = "",
-    val title: String = "",
-    val subtitle: String = "",
-    val songUrl: String = "",
-    val imageUrl: String = ""
-)
